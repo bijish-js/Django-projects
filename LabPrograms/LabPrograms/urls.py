86938668bd0fa3b1cf87eaecc73152f32195d02b
Bijish O B <bijish_js@outlook.com>
2021-08-30 12:57:42 +0530

    added superuser and configured home directory

--- LabPrograms/LabPrograms/urls.py
+++ LabPrograms/LabPrograms/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from .import views
+
 
 urlpatterns = [
+    
+    path('', views.projectHome,name='projectHome'),
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
     path('students/', include('students.urls')),
--- LabPrograms/students/admin.py
+++ LabPrograms/students/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Student
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('idno','name','mark1','mark2','mark3')
 
 # Register your models here.
+
+admin.site.register(Student,StudentAdmin)
+
